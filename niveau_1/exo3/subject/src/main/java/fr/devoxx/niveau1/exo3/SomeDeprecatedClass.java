c9596da198ae40b1bf32e2cc2011c8005f28d82c
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2015-02-17 16:06:00 +0100

    [#6] multimodule maven project with working code for niveau 1 exo 3

---
+++ niveau_1/exo3/subject/src/main/java/fr/devoxx/niveau1/exo3/SomeDeprecatedClass.java
@@ -0,0 +1,11 @@
+package fr.devoxx.niveau1.exo3;
+
+/**
+ * SomeDeprecatedClass - Une classe annotée avec {@link java.lang.Deprecated}, ce qui déclenchera l'annotation processor
+ * {@code HelloWorldProcessor}
+ *
+ * @author Sébastien Lesaint
+ */
+@Deprecated
+public class SomeDeprecatedClass {
+}
