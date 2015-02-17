2b631f3cc14fa1b1ebf43ed652aeb9a98452ef6a
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2015-02-17 16:04:55 +0100

    [#6] source files for niveau 1 exo 1

---
+++ niveau_1/javac/exo1/SomeDeprecatedClass.java
@@ -0,0 +1,9 @@
+/**
+ * SomeDeprecatedClass - Une classe annotée avec {@link Deprecated}, ce qui déclenchera l'annotation processor
+ * {@code HelloWorldProcessor}
+ *
+ * @author Sébastien Lesaint
+ */
+@Deprecated
+public class SomeDeprecatedClass {
+}
