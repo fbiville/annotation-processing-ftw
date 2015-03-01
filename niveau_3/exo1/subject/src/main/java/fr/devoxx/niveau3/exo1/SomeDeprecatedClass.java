514b15282ffaf39294b05bde816f01830f1310c3
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2015-03-01 18:12:27 +0100

    niveau3 directory skeleton

---
+++ niveau_3/exo1/subject/src/main/java/fr/devoxx/niveau3/exo1/SomeDeprecatedClass.java
@@ -0,0 +1,11 @@
+package fr.devoxx.niveau3.exo1;
+
+/**
+ * SomeDeprecatedClass - Une classe annotée avec {@link java.lang.Deprecated}, ce qui déclenchera l'annotation processor
+ * {@code HelloWorldProcessor}
+ *
+ * @author Sébastien Lesaint
+ */
+@Deprecated
+public class SomeDeprecatedClass {
+}
